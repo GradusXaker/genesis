2cae1b14dae1b413376120c834c4e1082505d3f4
GradusXaker <gradus_xaker@mail.ru>
2026-03-03 12:35:28 +0700

    Consolidate

---
+++ archived/docs.ubports.com/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# color codes
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+PLAIN='\033[0m'
+
+# check if virtualenv already exists
+if [ -d "$HOME/ubportsdocsenv" ]; then
+  echo -e "${GREEN}Build environment found.${PLAIN}"
+  . ~/ubportsdocsenv/bin/activate
+else
+  echo -e "${RED}No build environment found.${PLAIN}"
+  echo -e "${YELLOW}Installing pip and virtualenv.${PLAIN}"
+  sudo apt install python-pip
+  sudo -H pip install virtualenv
+  echo -e "${YELLOW}Creating a virtual environment in ${HOME}/ubportsdocsenv.${PLAIN}"
+  virtualenv ~/ubportsdocsenv
+  . ~/ubportsdocsenv/bin/activate
+  echo -e "${YELLOW}Installing build tools and prerequisites.${PLAIN}"
+  pip install -r requirements.txt
+fi
+echo -e "${GREEN}Building...${PLAIN}"
+if [ "$(uname)" == "Linux" ]; then
+  sphinx-build -Wa . _build/html -j `nproc --all`
+else
+  sphinx-build -Wa . _build/html -j `sysctl -n hw.ncpu`
+fi
+exit $?
